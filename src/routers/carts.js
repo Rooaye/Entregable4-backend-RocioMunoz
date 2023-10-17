e2f11432de351c55b119ca389cd018c75b94be8a
Rocio Muñoz <ayelen.ro.96m@gmail.com>
2023-10-17 17:50:52 -0300

    entregable4

---
+++ src/routers/carts.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+
+
+import CartManager from '../CartsManager.js';
+const cartManager= new CartManager();
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+  const carts = JSON.parse(await cartManager.getCarts());
+  return res.status(200).json(carts);
+}) 
+
+router.post('/', async (req, res) => {
+  await cartManager.addCart(req.body)
+  res.status(201).json(req.body); 
+});
+
+router.get('/:cid', async (req, res) => {
+  const carts = JSON.parse(await cartManager.getCarts());
+  const cart = carts.find(cart => cart.id === req.params.cid)
+  return cart? res.status(200).json(cart): res.status(404).json({ error: 'Cart not found' });
+});
+
+router.post('/:cid/product/:pid', async (req, res) => {
+  const carts = JSON.parse(await cartManager.getCarts());
+  const cart = carts.find(cart => cart.id === req.params.cid)
+  if (!cart) {
+    res.status(404).json({ message: 'cart no found' }); 
+  } else {
+    const productId = req.params.pid;
+    const product = { id: productId, quantity: 1 }; 
+    const existingProduct = cart.products.find(p => p.id === productId); 
+    if (existingProduct) {
+      existingProduct.quantity++; 
+    } else {
+      cart.products.push(product);
+    }
+    res.json(cart.products); 
+    await cartManager.updateCart(req.params.cid, cart);
+  }
+});
+
+
+export default router;
